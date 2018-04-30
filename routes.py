c9bab2389ae0e08caa86316d546e273e9b666d6a
Daniel Garcia <danrhumphries@gmail.com>
2018-04-30 06:50:16 -0700

    Create a SQL database as well as a model to use the database.

--- routes.py
+++ routes.py
@@ -1,17 +1,26 @@
 from flask import Flask, render_template
+from models import db
 
+# Start app
 app = Flask(__name__)
 
+# Connect to SQL database
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgressql://localhost/learningflask"
+db.init_app(app)
 
+
+# Route to index
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
+# Route to about page
 @app.route("/about")
 def about():
     return render_template("about.html")
 
 
+# Main entrance
 if __name__ == "__main__":
     app.run(debug=True)
